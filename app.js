5a0c9ea27951452cbb7db674b3242e3e6614231d
comunit <comunit76@gmail.com>
2017-10-21 01:04:02 +0100

    initial commit

---
+++ app.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const authRoutes = require('./routes/auth-routes');
+const passportSetup = require('./config/passport-setup');
+const mongoose = require('mongoose');
+const keys = require('./config/keys');
+
+const app = express();
+
+// setup view engine
+app.set('view engine', 'ejs');
+
+//connect to mongo db
+mongoose.connect(keys.mongodb.dbURI, () => {
+  console.log('connect to mongo db');
+});
+
+//setup routes
+app.use('/auth', authRoutes);
+
+//create home route
+app.get('/', (req, res) => {
+  res.render('home');
+})
+
+app.listen(3000, () => {
+  console.log('App now listening to port 3000');
+});
+
