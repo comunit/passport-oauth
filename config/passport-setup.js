2fb4943660cfab7f9478da817fd253a27545c2da
comunit <comunit76@gmail.com>
2017-10-22 18:36:18 +0100

    saving data to mongo db

--- config/passport-setup.js
+++ config/passport-setup.js
@@ -1,6 +1,7 @@
 const passport = require('passport');
 const GoogleStrategy = require('passport-google-oauth20');
 const keys = require('./keys');
+const User = require('../models/user-model');
 
 passport.use(
     new GoogleStrategy({
@@ -9,7 +10,11 @@ passport.use(
         clientSecret: keys.google.clientSecret,
         callbackURL: '/auth/google/redirect'
     }, (accessToken, refreshToken, profile, done) => {
-        console.log('passport call function fired');
-        console.log(profile);
+        new User({
+          username: profile.displayName,
+          googleid: profile.id
+        }).save().then((newUser) => {
+          console.log('new User Created' + newUser);
+        })
     })
 );
